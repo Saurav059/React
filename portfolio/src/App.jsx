fa074359594ba9b9c43b57acf5f66d464c4e69d8
Saurav Devkota <saurav.devkota.2002@gmail.com>
2024-07-20 02:06:10 +0530

    Skills page done

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 import Hero from './Sections/Hero/Hero';
+import Skills from './Sections/Skills/Skills';
+
+
 function App() {
   
   return (
     <>
       <Hero />
+      <Skills />
     </>
   );
 
